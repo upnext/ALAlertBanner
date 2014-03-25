b1fae360e2a37dcec2d28aabca2fa2dea8a2b0d6
marcinpiesiak <marcin.piesiak@gmail.com>
2014-03-25 09:05:02 +0100

    added new style part 2

--- ALAlertBanner/ALAlertBanner.h
+++ ALAlertBanner/ALAlertBanner.h
@@ -30,7 +30,7 @@ typedef enum {
     ALAlertBannerStyleFailure,
     ALAlertBannerStyleNotify,
     ALAlertBannerStyleWarning,
-    
+    ALAlertBannerStylePushNotif,
 } ALAlertBannerStyle;
 
 typedef enum {
